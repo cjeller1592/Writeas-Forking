5f6bb65bade40d27efb76d6dbf668ffe08d208d9
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2019-02-21 21:50:37 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, flash, request, render_template, redirect, url_for
+
+from auth import loginUser
+
+from utils import forkCPost, getWiki, block2WA, yt2W
+from forms import login, Fork, Wiki, Arena, Youtube
+import os
+import wikipedia
+
+
+app = Flask(__name__)
+
+SECRET_KEY = os.urandom(32)
+
+app.config['SECRET_KEY'] = SECRET_KEY
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def start():
+    global token # Setting token the stupid way!
+
+    form = login()
+    error = None
+
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        token = loginUser(user, password)
+
+        if token == None:
+            flash('Woops! Try again!')
+#            return redirect(url_for('start'))
+            return redirect(request.url)
+
+        flash('You successfully logged in! You can start using the app!')
+        return redirect(url_for('home'))
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/fork', methods=['GET', 'POST'])
+def fork():
+    form = Fork()
+    if request.method == 'POST':
+        try:
+            alias = request.form['alias']
+            slug = request.form['slug']
+
+            url = forkCPost(alias, slug, token)
+
+            if url == None: # catches if alias/slug are off in request
+                flash('Hmm something went wrong. Check the alias and or slug!')
+                return redirect(url_for('fork'))
+
+            return redirect(url)
+
+        except NameError: # catches if someone tries to use app before logging in
+                flash('Easy tiger! Login first . . .')
+                return redirect(url_for('start'))
+
+
+    return render_template('fork.html', form=form)
